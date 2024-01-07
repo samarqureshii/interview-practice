77f46e2159eedfbf2dbeedb93d385228cea7f721
samarqureshii <samarq713@gmail.com>
2024-01-07 16:20:13 -0500

    did xcode mess the format?

---
+++ interview-practice/interview-practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  interview-practice
+//
+//  Created by Samar Qureshi on 2024-01-07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
