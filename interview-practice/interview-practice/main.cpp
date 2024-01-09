48fe020db712b6a53810f566ea5bc8a00b264208
samarqureshii <samarq713@gmail.com>
2024-01-09 13:56:57 -0500

    Mutexes and Locks

--- interview-practice/interview-practice/main.cpp
+++ interview-practice/interview-practice/main.cpp
@@ -10,16 +10,53 @@
 #include <sstream>
 #include <thread>
 #include <mutex>
+
 using namespace std;
 
 class BankAccount {
     // ... Class implementation
+    public:
+    int balance;
+    mutex mtx; //synchronized access mutex
+    
+    BankAccount(){ //constructor
+        balance = 0;
+    }
+    
+    void deposit(int val){
+        lock_guard<mutex> lock(mtx); //lock the mutex
+        balance+=val;
+    }
+    
+    bool withdraw(int val){
+        lock_guard<mutex> lock(mtx); //lock the mutex
+        //return true if we can withdraw the balance, return false if the balance is so low that we cannot withdraw anything from it
+        if(balance - val >=0){
+            balance-=val;
+            return true;
+        }
+        else{ // we cannot return any balance
+            return false;
+        }
+    }
+    
 };
+
+void bankingOperation(BankAccount& account, int depositAmount, int withdrawAmount) {
+    account.deposit(depositAmount);
+    if (!account.withdraw(withdrawAmount)) { //if we cannot withdraw bc we are broke
+        cout << "Failed to withdraw: " << withdrawAmount << endl;
+    }
+}
+
 
 int main() {
     // Create BankAccount object
+    BankAccount account;
 
     // Create and start threads
+    thread t1(bankingOperation, ref(account), 500, 100);
+    
 
     // Join threads
 
