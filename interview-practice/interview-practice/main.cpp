d17147386a239926b4e95456d7de73f3d48d42dc
samarqureshii <samarq713@gmail.com>
2024-01-16 09:11:15 -0500

    led code

--- interview-practice/interview-practice/main.cpp
+++ interview-practice/interview-practice/main.cpp
@@ -15,49 +15,45 @@
 #include <random>
 #include <atomic>
 #include <stack>
+#include <vector>
 
 using namespace std;
 
-//varint decoder... again
-
-vector<uint32_t> decode_varint(vector<uint8_t>& input){
-    vector<uint32_t> output;
-    int curr = 0;
-    int shift = 0;
-    
-    for(uint8_t byte : input){ //iterate through every 8 bit byte in the input
-        //if the MSB in the byte is set
-        if(byte & 0x80){
-            //then extract the 7 MSB and append to the current varint
-            curr|= static_cast<uint32_t>(byte & 0x7F) << shift;
-            shift+=7;
-            
-            if(shift == 28 && (byte & 0xF0)){ //at this point, we have gone through 4 bytes and are on the 5th one. we can only extract 4 more meaningful bits of data from the fifth byte. anything beyond this would indicate overflow. we can check this by seeing if the 4 <Sb in the final byte are set or not
-                return vector<uint32_t>(); //return empty vector
-            }
-        }
-        //if the MSB in the byte is not set, we are at the last byte
-        else{
-            //finish off the current varint
-            curr|= static_cast<uint32_t>(byte & 0x7F) << shift;
-            //and append it to the output vector
-            output.push_back(curr);
-            //and reset these for the next varint in the input
-            shift = 0;
-            curr = 0;
-            
-        }
-        //check for an incomplete varint
-        if(!input.empty() && (input.back() & 0x80)){
-            return vector<uint32_t>();
-        }
-        
-        return output;
+//You are required to implement a thread-safe queue in C++ that will be used in a high-performance application where multiple threads will be pushing and popping items concurrently. The queue should be dynamically allocated and should have the capability to expand if more items are added than its initial size.
+class threadSafeQueue{
+public:
+    int elements;
+    threadSafeQueue(int s){ //constructor
+        mutex mtx;
+        elements = s;
+        new arr[size];
     }
     
+    ~threadSafeQueue(){ //destructor
+        delete[];
+    }
+    
+    void push(int item){
+        //Inserts an item at the end of the queue.
+        lock_guard<mutex> lock(mtx); //how to lock the mutex?
+        
+        //The queue should expand its capacity when it runs out of space by allocating a new array that is double the size, copying over the existing elements, and then deallocating the old array.
 
+        
+    }
     
+    int pop(){
+        // Removes an item from the front of the queue and returns it.
+        lock_guard<mutex> lock(mtx);
+    }
     
-    return output;
+    size_t size(){ //returns the number of elements in the queue;
+        lock_guard<mutex> lock(mtx);
+        return elements;
+    }
+};
+
+int main(){
+    threadSafeQueue queue;
 }
 
