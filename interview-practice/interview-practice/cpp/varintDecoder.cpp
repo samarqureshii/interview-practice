0b420845ba420d5f3520bb614f047212e94c0995
samarqureshii <samarq713@gmail.com>
2024-01-15 19:42:07 -0500

    now we're balling!

--- interview-practice/interview-practice/cpp/varintDecoder.cpp
+++ interview-practice/interview-practice/cpp/varintDecoder.cpp
@@ -1,54 +1,63 @@
 //
-//  varintDecoder.cpp
+//  main.cpp
 //  interview-practice
 //
-//  Created by Samar Qureshi on 2024-01-11.
+//  Created by Samar Qureshi on 2024-01-07.
 //
 
 #include <stdio.h>
 #include <iostream>
+#include <sstream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <queue>
+#include <random>
+#include <atomic>
+#include <stack>
+
 using namespace std;
 
+//varint decoder... again
 
-vector<uint32_t> decode_varint(vector<uint8_t>& input){ //pass input by reference
-    int shift = 0;
+vector<uint32_t> decode_varint(vector<uint8_t>& input){
+    vector<uint32_t> output;
     int curr = 0;
-    vector <uint32_t> output; //the output that we will return
+    int shift = 0;
     
-    //loop over each byte in the input vector
-    for(uint8_t byte : input){
-        //check the continuation (8th) bit
+    for(uint8_t byte : input){ //iterate through every 8 bit byte in the input
+        //if the MSB in the byte is set
         if(byte & 0x80){
-            //extract the 7 LSB (8th bit is used as a continuation flag)
-            //combine the current value with the bits from the byte
+            //then extract the 7 MSB and append to the current varint
             curr|= static_cast<uint32_t>(byte & 0x7F) << shift;
             shift+=7;
             
-            //overflow check here
-            if(shift > 28 && !(byte & 0xF0)){ //on the fifth byte is where we could have potential overflow
-                //we can concatenate a fifth byte, but only the LSB 4 bits, anything more would mean overflow
-                shift = 0;
-                curr = 0;
-                return vector<uint32_t>();
+            if(shift == 28 && (byte & 0xF0)){ //at this point, we have gone through 4 bytes and are on the 5th one. we can only extract 4 more meaningful bits of data from the fifth byte. anything beyond this would indicate overflow. we can check this by seeing if the 4 <Sb in the final byte are set or not
+                return vector<uint32_t>(); //return empty vector
             }
-
+        }
+        //if the MSB in the byte is not set, we are at the last byte
+        else{
+            //finish off the current varint
+            curr|= static_cast<uint32_t>(byte & 0x7F) << shift;
+            //and append it to the output vector
+            output.push_back(curr);
+            //and reset these for the next varint in the input
+            shift = 0;
+            curr = 0;
+            
+        }
+        //check for an incomplete varint
+        if(!input.empty() && (input.back() & 0x80)){
+            return vector<uint32_t>();
         }
         
-        else{ //the continuation bit is not set, we are at a byte like 0xxxxxxx which is the last byte of the varint
-            //append curr to the output
-            curr|= static_cast<uint32_t>(byte & 0x7F) << shift;
-            output.push_back(curr);
-            curr = 0;
-            shift = 0;
-            
-            
-        }
+        return output;
     }
     
-    //did the input end on a case where there is more data we just couldn't fit it in 32 bits,
-    if(!input.empty() && (input.back() & 0x80)){
-        return vector<uint32_t>();
-    }
+
+    
     
     return output;
 }
+
--- interview-practice/interview-practice/main.cpp
+++ interview-practice/interview-practice/main.cpp
@@ -18,3 +18,46 @@
 
 using namespace std;
 
+//varint decoder... again
+
+vector<uint32_t> decode_varint(vector<uint8_t>& input){
+    vector<uint32_t> output;
+    int curr = 0;
+    int shift = 0;
+    
+    for(uint8_t byte : input){ //iterate through every 8 bit byte in the input
+        //if the MSB in the byte is set
+        if(byte & 0x80){
+            //then extract the 7 MSB and append to the current varint
+            curr|= static_cast<uint32_t>(byte & 0x7F) << shift;
+            shift+=7;
+            
+            if(shift == 28 && (byte & 0xF0)){ //at this point, we have gone through 4 bytes and are on the 5th one. we can only extract 4 more meaningful bits of data from the fifth byte. anything beyond this would indicate overflow. we can check this by seeing if the 4 <Sb in the final byte are set or not
+                return vector<uint32_t>(); //return empty vector
+            }
+        }
+        //if the MSB in the byte is not set, we are at the last byte
+        else{
+            //finish off the current varint
+            curr|= static_cast<uint32_t>(byte & 0x7F) << shift;
+            //and append it to the output vector
+            output.push_back(curr);
+            //and reset these for the next varint in the input
+            shift = 0;
+            curr = 0;
+            
+        }
+        //check for an incomplete varint
+        if(!input.empty() && (input.back() & 0x80)){
+            return vector<uint32_t>();
+        }
+        
+        return output;
+    }
+    
+
+    
+    
+    return output;
+}
+
